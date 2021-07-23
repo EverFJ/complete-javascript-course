b978a1b444c988f50c5439a43e6d5c1363f76c63
Francois Janson <jansonfrancois@gmail.com>
2021-07-23 18:21:33 +0200

    Exercises update

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,17 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const one = [17, 21, 23];
+const two = [12, 5, -5, 0, 4];
+
+const printForecast = function (arr) {
+  let string = "";
+
+  for (let i = 0; i < arr.length; i++) {
+    string += `${arr[i]}° in ${i + 1} days... `;
+  }
+  return string;
+};
+
+console.log(printForecast(one));
+console.log(printForecast(two));
--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+
+let message = (document.querySelector('.message').textContent =
+  'Correct number');
+
+let number = (document.querySelector('.number').textContent = 15);
+let score = (document.querySelector('.score').textContent = 15);
+// let input = document.querySelector('.guess').value;
+let btn = document.querySelector('.check');
+console.log(btn);
+
+btn.addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess);
+
+  if (!guess) {
+    console.log('No number !');
+  }
+});
