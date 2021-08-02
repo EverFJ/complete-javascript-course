63539fe9eddf2f3b630176486c7c99365d700b78
Francois Janson <jansonfrancois@gmail.com>
2021-08-03 00:38:24 +0200

    Variable scopes

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function calcAge(birthYear) {
+    const age = 2017 - birthYear;
+
+    function printAge() {
+        const output = `${firstName}, you are ${age}, born in ${birthYear}`
+        console.log(output);
+        if (birthYear <= 2000 && birthYear >= 1983) {
+            const str = `Oh, and you're a millenial ${firstName}`
+            console.log(str);
+        }
+    }
+    printAge();
+    return age;
+}
+
+const firstName = "François";
+// console.log(printAge())
+calcAge(1986);
